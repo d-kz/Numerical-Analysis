4c58c6bde4269d71f86c6892f9a9cdc511c5383a
94kazakov <94kazakov@gmail.com>
2015-12-01 16:33:04 -0700

    working rk 4 part 1

--- Project2/project2.py
+++ Project2/project2.py
@@ -28,25 +28,25 @@ def rk4(q1, q2, q3, q4, q5, h, Pr):
 
         U1k1 = h * U1prime(t, q2)
         U2k1 = h * U2prime(t, q3)
-        U3k1 = h * U3prime(t, q1, q2)
+        U3k1 = h * U3prime(t, q1, q3)
         U4k1 = h * U4prime(t, q5)
         U5k1 = h * U5prime(t, q1, q5, Pr)
 
         U1k2 = h * U1prime(t + (0.5*h), q2 + (0.5*U2k1))
         U2k2 = h * U2prime(t + (0.5*h), q3 + (0.5*U3k1))
-        U3k2 = h * U3prime(t + (0.5*h), q1 + (0.5*U1k1), q2 + (0.5*U2k1))
+        U3k2 = h * U3prime(t + (0.5*h), q1 + (0.5*U1k1), q3 + (0.5*U3k1))
         U4k2 = h * U4prime(t + (0.5*h), q5 + (0.5*U5k1))
         U5k2 = h * U5prime(t + (0.5*h), q1 + (0.5*U1k1), q5 + (0.5*U5k1), Pr)
 
         U1k3 = h * U1prime(t + (0.5*h), q2 + (0.5*U2k2))
         U2k3 = h * U2prime(t + (0.5*h), q3 + (0.5*U3k2))
-        U3k3 = h * U3prime(t + (0.5*h), q1 + (0.5*U1k2), q2 + (0.5*U2k2))
+        U3k3 = h * U3prime(t + (0.5*h), q1 + (0.5*U1k2), q3 + (0.5*U3k2))
         U4k3 = h * U4prime(t + (0.5*h), q5 + (0.5*U5k2))
         U5k3 = h * U5prime(t + (0.5*h), q1 + (0.5*U1k2), q5 + (0.5*U5k2), Pr)
         
         U1k4 = h * U1prime(t + h, q2 + U2k3)
         U2k4 = h * U2prime(t + h, q3 + U3k3)
-        U3k4 = h * U3prime(t + h, q1 + U1k3, q2 + U2k3)
+        U3k4 = h * U3prime(t + h, q1 + U1k3, q3 + U3k3)
         U4k4 = h * U4prime(t + h, q5 + U5k3)
         U5k4 = h * U5prime(t + h, q1 + U1k3, q5 + U5k3, Pr)
         
@@ -66,8 +66,8 @@ def U1prime(t,q2):
 def U2prime(t,q3):
     return q3
 
-def U3prime(t, q1, q2):
-    return -1.0/2.0 * q1 * q2
+def U3prime(t, q1, q3):
+    return -1.0/2.0 * q1 * q3
 
 def U4prime(t, q5):
     return q5
@@ -77,33 +77,36 @@ def U5prime(t, q1, q5, Pr):
 
 
 def part1():
-    u3 = 0.59 
+    u3 = 0.332056861162 # error =  -9.90517633026e-07
     change = [u3, float("inf")]
     der = [0.0,0.0]
-    #while abs(change[1] - change[0]) > 0.0000001:
-    h = 0.1
-    u1 = 0
-    u2 = 0
-    u4 = 1
-    u5 = guessU5()
-    Pr = 5
-    points = rk4(u1, u2, u3, u4, u5, h, Pr)
-    change[0] = u3
-    der[0] = der[1]
-    der[1] = points[2][-1]
-    forceCheck = len(points[2]) - int(random.random() * (50)) - 1
-    u3 += -0.01 * (der[1] - der[0]) * (points[2][forceCheck] - 1) 
-    change[1] = u3
-    print "for {}, change of u:{} ERROR: {}, forceCheck{}".format(u3, (change[1] - change[0]), points[2][-1] - 1, forceCheck)
+    while abs(change[1] - change[0]) > 0.00000001:
+        h = 0.1
+        u1 = 0
+        u2 = 0
+        u4 = 1
+        u5 = guessU5()
+        Pr = 5
+        points = rk4(u1, u2, u3, u4, u5, h, Pr)
+        change[0] = u3
+        #forceCheck = len(points[2]) - int(random.random() * (50)) - 1
+        forceCheck = len(points[2]) - 1
+        u3 += -0.01 * (points[2][forceCheck] - 1) 
+        change[1] = u3
+        print "for {}, change of u:{} ERROR: {}, forceCheck{}".format(u3, (change[1] - change[0]), points[2][-1] - 1, forceCheck)
     
     plt.plot(points[0], points[1], '#ED5377', label="F")
     plt.plot(points[0], points[2], '#FF0000', label="F\'")
     plt.plot(points[0], points[3], '#FFB8B9', label="F\'\'")
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.show()
+
+    
+
     plt.plot(points[0], points[4], '#004746', label="G")
     plt.plot(points[0], points[5], '#799493', label="G\'")
     plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
     plt.show()
-
     return [points[1], points[2], points[0]]
 
 
